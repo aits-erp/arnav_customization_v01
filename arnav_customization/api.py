926dac04665b00d02f5cc93218b75e6ab38bf5e9
aits-erp <backup@aitsind.com>
2026-02-18 11:18:21 +0530

    Update api.py
    change the data

--- arnav_customization/api.py
+++ arnav_customization/api.py
@@ -1,38 +1,16 @@
 import frappe
 
 @frappe.whitelist()
-def create_sku_master(
-    product,
-    sku,
-    qty=None,
-    cost_price=None,
-    selling_price=None,
-    image=None,
-    gross_weight=None,
-    net_weight=None,
-    breakup=None
-):
+def create_sku_via_api(product, sku):
 
-    # ⭐ Parent Doc Create
     doc = frappe.new_doc("SKU")
 
-    # ⭐ Child Table Row Add
     doc.append("sku_details", {
         "product": product,
-        "sku": sku,
-        "qty": qty,
-        "cost_price": cost_price,
-        "selling_price": selling_price,
-        "image": image,
-        "gross_weight": gross_weight,
-        "net_weight": net_weight,
-        "breakup": breakup
+        "sku": sku
     })
 
     doc.insert(ignore_permissions=True)
     doc.submit()
 
-    return {
-        "status": "success",
-        "name": doc.name
-    }
+    return {"status": "success", "name": doc.name}
