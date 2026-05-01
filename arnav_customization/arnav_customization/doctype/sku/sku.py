060882ee605d5a2c86b9d3c79be65248849816bf
uNik020 <shekharnikhil020@gmail.com>
2026-05-01 06:38:40 +0000

    sku d_no fetching

--- arnav_customization/arnav_customization/doctype/sku/sku.py
+++ arnav_customization/arnav_customization/doctype/sku/sku.py
@@ -1,9 +1,29 @@
-# Copyright (c) 2026, aits and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SKU(Document):
-	pass
+
+    def before_save(self):
+        self.set_d_no()
+
+    def onload(self):
+        # ensures value is visible even if old record is opened
+        self.set_d_no()
+
+    def set_d_no(self):
+        if not self.sku_master or not self.name:
+            return
+
+        # Fetch d_no from SKU Details child table
+        d_no = frappe.db.get_value(
+            "SKU Details",
+            {
+                "parent": self.sku_master,
+                "sku": self.name   # your field is DATA, not link
+            },
+            "d_no"
+        )
+
+        if d_no:
+            self.d_no = d_no
