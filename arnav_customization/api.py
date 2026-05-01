81c831a3a01fd7e6ec3e6b0c347373da9546c59d
uNik020 <shekharnikhil020@gmail.com>
2026-05-01 13:37:51 +0000

    query totally changed

--- arnav_customization/api.py
+++ arnav_customization/api.py
@@ -166,32 +166,51 @@ def get_sku_details(warehouse=None):
     #     GROUP BY b.item_code
     # """, {"warehouse": warehouse}, as_dict=True)
 
+    # sku_details = frappe.db.sql("""
+	#     SELECT
+	#         sd.name AS sku_name,
+	#         sd.sku,
+	#         sd.product,
+	#         i.item_name,
+	#         IFNULL(b.actual_qty, 0) AS qty,
+	#         sd.selling_price,
+	#         sd.gross_weight,
+	#         sd.net_weight,
+	#         sd.huid,
+	#         sd.d_no,
+	#         sd.image
+	
+	#     FROM `tabSKU Details` sd
+	
+	#     INNER JOIN `tabItem` i
+	#         ON i.name = sd.product
+	
+	#     LEFT JOIN `tabBin` b
+	#         ON b.item_code = sd.product
+	#         AND b.warehouse = %(warehouse)s
+	
+	#     WHERE IFNULL(b.actual_qty, 0) > 0
+	
+	# """, {"warehouse": warehouse}, as_dict=True)
+
     sku_details = frappe.db.sql("""
-	    SELECT
-	        sd.name AS sku_name,
-	        sd.sku,
-	        sd.product,
-	        i.item_name,
-	        IFNULL(b.actual_qty, 0) AS qty,
-	        sd.selling_price,
-	        sd.gross_weight,
-	        sd.net_weight,
-	        sd.huid,
-	        sd.d_no,
-	        sd.image
-	
-	    FROM `tabSKU Details` sd
-	
-	    INNER JOIN `tabItem` i
-	        ON i.name = sd.product
-	
-	    LEFT JOIN `tabBin` b
-	        ON b.item_code = sd.product
-	        AND b.warehouse = %(warehouse)s
-	
-	    WHERE IFNULL(b.actual_qty, 0) > 0
-	
-	""", {"warehouse": warehouse}, as_dict=True)
+        SELECT
+            s.name AS sku_name,
+            s.sku,
+            s.product,
+            1 AS qty,
+            s.selling_price,
+            s.gross_weight,
+            s.net_weight,
+            s.huid,
+            s.d_no,
+            s.image
+
+        FROM `tabSKU` s
+
+        WHERE s.warehouse = %(warehouse)s
+
+    """, {"warehouse": warehouse}, as_dict=True)
 
     for item in sku_details:
 
