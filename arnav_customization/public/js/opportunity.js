6aa0145514e61e58d6fcf2ef7ce4581be95c6f5d
uNik020 <shekharnikhil020@gmail.com>
2026-05-08 09:10:37 +0000

    opportunity js improved

--- arnav_customization/public/js/opportunity.js
+++ arnav_customization/public/js/opportunity.js
@@ -1,9 +1,12 @@
 frappe.ui.form.on("Opportunity", {
-    refresh: function(frm) {
+    refresh(frm) {
 
         if (frm.is_new()) return;
 
-        // Sales Invoice Button
+        // Prevent duplicate buttons
+        frm.clear_custom_buttons();
+
+        // Sales Invoice
         frm.add_custom_button(__("Sales Invoice"), function () {
 
             frappe.new_doc("Sales Invoice", {
@@ -12,36 +15,76 @@ frappe.ui.form.on("Opportunity", {
                 opportunity_title: frm.doc.title
             });
 
-        }, __("Create"));
+        });
 
-
-        // POS Button
+        // POS
         frm.add_custom_button(__("POS"), function () {
 
-            frappe.model.with_doctype("POS", function () {
+            frappe.new_doc("POS", {
+                client_name: frm.doc.customer_name || frm.doc.title || "",
+                email: frm.doc.contact_email || "",
+                opportunity: frm.doc.name,
 
-                let doc = frappe.model.get_new_doc("POS");
+                lead_owner_1: frm.doc.custom_lead_owner_1 || "",
+                lead_owner_2: frm.doc.custom_lead_owner_2 || "",
 
-                // ===== FIELD MAPPING =====
-                doc.client_name = frm.doc.customer_name || frm.doc.title || "";
-                doc.email = frm.doc.contact_email || "";
-                doc.opportunity = frm.doc.name;
+                salestype: frm.doc.custom_lead_source || "",
+                billtype: frm.doc.custom_metal_interest || "",
 
-                doc.lead_owner_1 = frm.doc.custom_lead_owner_1 || "";
-                doc.lead_owner_2 = frm.doc.custom_lead_owner_2 || "";
-
-                doc.salestype = frm.doc.custom_lead_source || "";
-                doc.billtype = frm.doc.custom_metal_interest || "";
-
-                doc.mobile_number = frm.doc.whatsapp || frm.doc.contact_mobile || frm.doc.phone || "";
-                doc.pincode = frm.doc.custom_pin_code || "";
-                doc.address = frm.doc.custom_address || "";
-
-                frappe.set_route("Form", "POS", doc.name);
-
+                mobile_number: frm.doc.whatsapp || frm.doc.contact_mobile || frm.doc.phone || "",
+                pincode: frm.doc.custom_pin_code || "",
+                address: frm.doc.custom_address || ""
             });
 
-        }, __("Create"));
+        });
 
     }
 });
+
+// frappe.ui.form.on("Opportunity", {
+//     refresh: function(frm) {
+
+//         if (frm.is_new()) return;
+
+//         // Sales Invoice Button
+//         frm.add_custom_button(__("Sales Invoice"), function () {
+
+//             frappe.new_doc("Sales Invoice", {
+//                 customer: frm.doc.customer_name || frm.doc.title,
+//                 opportunity: frm.doc.name,
+//                 opportunity_title: frm.doc.title
+//             });
+
+//         }, __("Create"));
+
+
+//         // POS Button
+//         frm.add_custom_button(__("POS"), function () {
+
+//             frappe.model.with_doctype("POS", function () {
+
+//                 let doc = frappe.model.get_new_doc("POS");
+
+//                 // ===== FIELD MAPPING =====
+//                 doc.client_name = frm.doc.customer_name || frm.doc.title || "";
+//                 doc.email = frm.doc.contact_email || "";
+//                 doc.opportunity = frm.doc.name;
+
+//                 doc.lead_owner_1 = frm.doc.custom_lead_owner_1 || "";
+//                 doc.lead_owner_2 = frm.doc.custom_lead_owner_2 || "";
+
+//                 doc.salestype = frm.doc.custom_lead_source || "";
+//                 doc.billtype = frm.doc.custom_metal_interest || "";
+
+//                 doc.mobile_number = frm.doc.whatsapp || frm.doc.contact_mobile || frm.doc.phone || "";
+//                 doc.pincode = frm.doc.custom_pin_code || "";
+//                 doc.address = frm.doc.custom_address || "";
+
+//                 frappe.set_route("Form", "POS", doc.name);
+
+//             });
+
+//         }, __("Create"));
+
+//     }
+// });
